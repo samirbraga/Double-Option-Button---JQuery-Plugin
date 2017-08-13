46f1c721a742d6f0e7d9083cd4522a0f89688bef
Samir Braga <samirchavess@gmail.com>
2017-08-13 13:26:09 -0300

    Fix bugs

--- src/dbl-option.js
+++ src/dbl-option.js
@@ -6,13 +6,16 @@
 $.fn.dblOption = function(opts){
 	var self = $(this);
 
-  self.addClass('dbl-option-container ' + (opts['initSide'] || "right"));
+  $.fn.dblOption.options = $.extend({}, $.fn.dblOption.defaults, opts);
+  opts = $.fn.dblOption.options;
 
-  if (opts['callOnInit'] == true || opts['callOnInit'] == undefined) {
-    if ((opts['initSide'] || "right") == 'right') {
-			if (opts['onRight'] && opts['onRight'] instanceof Function) opts['onRight']();
+  self.addClass('dbl-option-container ' + opts['initSide']);
+
+  if (opts['callOnInit'] == true) {
+    if (opts['initSide'] == 'right') {
+			opts['onRight']();
     } else {
-			if (opts['onLeft'] && opts['onLeft'] instanceof Function) opts['onLeft']();
+			opts['onLeft']();
     }
   }
 
@@ -44,36 +47,70 @@ $.fn.dblOption = function(opts){
 
   self.append(template);
 
-  self.find('.labels .label').css('width', parseFloat(opts['width'] || 220)/2 + 'px')
-  self.find('.labels-ranger').css('background', opts['bgSelector'] || '#363b44')
-  if (!!opts['animation'] || opts['animation'] == undefined) self.find('.labels-ranger').addClass('animated');
-  if (!!opts['animation'] || opts['animation'] == undefined) self.find('.labels-front').addClass('animated');
+  self.find('.labels .label').css('width', parseFloat(opts['width'])/2 + 'px')
+  self.find('.labels-ranger').css('background', opts['bgSelector'])
+  if (opts['animation'] == true) {
+  	self.find('.labels-ranger').addClass('animated');
+    self.find('.labels-front').addClass('animated');
+  }
 
   self.mousedown(function(e){
     e.preventDefault();
     return false;
   })
 
-  self.find('.label-right span').text((opts['rightLabel'] || ''));
-  self.find('.label-left span').text((opts['leftLabel'] || ''));
+  self.find('.label-right span').text(opts['rightLabel']);
+  self.find('.label-left span').text(opts['leftLabel']);
 
   var toggleOption = function(e){
     e.preventDefault();
 
-    var self = $(this);
+    var label = $(this);
     var option = $(this).closest('.dbl-option-container');
 
-    if (self.hasClass('label-right')) {
-      option.removeClass('right')
-      			.addClass('left');
-      if (opts['onLeft'] && opts['onLeft'] instanceof Function) opts['onLeft']();
+    if (label.hasClass('label-right')) {
+      self.setSide('right');
     } else {
-      option.removeClass('left')
-      			.addClass('right');
-      if (opts['onRight'] && opts['onRight'] instanceof Function) opts['onRight']();
+      self.setSide('left');
     }
     return false;
   }
 
   self.find('.labels-back .label').click(toggleOption);
+}
+
+$.fn.dblOption.defaults = {
+  width: '200px',
+  height: '30px',
+  initSide: 'left',
+  leftLabel: 'OPÇÃO 1',
+  rightLabel: 'OPÇÃO 2',
+  callOnInit: true,
+  bgSelector: '#363b44',
+  animation: true,
+  onChange: function () {
+  },
+  onRight: function () {
+    console.log('right');
+  },
+  onLeft: function () {
+    console.log('left');
+  }
+}
+
+$.fn.setSide = function(side){
+	var self = $(this);
+
+  if (self.hasClass('dbl-option-container') && !!side) {
+  	$.fn.dblOption.options['onChange']()
+
+  	side = side.toLowerCase();
+  	self.removeClass('right left');
+  	self.addClass(side);
+    if (side == 'right') {
+    	$.fn.dblOption.options['onRight']()
+    } else {
+    	$.fn.dblOption.options['onLeft']();
+    }
+  }
 }
